890399689ac7998751b465c46108408b22babb0a
PKENE <smithkeyz51@gmail.com>
2026-09-18 01:27:18 +0100

    Update LiveMap.jsx

--- phone-tracker/src/components/LiveMap.jsx
+++ phone-tracker/src/components/LiveMap.jsx
@@ -27,8 +27,10 @@ export default function LiveMap({ location }) {
   return (
     <MapContainer center={center} zoom={zoom} className="map-container" scrollWheelZoom zoomControl>
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
       />
       {hasLocation && (
         <>
